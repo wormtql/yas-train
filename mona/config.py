2e99614e615865cd0e1bdc941fdcd4ac53faa0a9
Jimmy <jimmy.jie.you@gmail.com>
2021-11-13 16:32:08 -0500

    Add new configs

--- mona/config.py
+++ mona/config.py
@@ -1,7 +1,7 @@
 config = {
     "height": 32,
     "train_width": 384,
-    "batch_size": 64,
+    "batch_size": 32,
     "epoch": 10,
     "print_per": 100,
     "save_per": 300,
@@ -11,4 +11,10 @@ config = {
 
     "pretrain": False,
     "pretrain_name": "chs_all.pt",
+
+    # Set according to your CPU
+    "dataloader_workers": 4,
+    # Generate data online for train/val
+    "online_train": True,
+    "online_val": True,
 }
