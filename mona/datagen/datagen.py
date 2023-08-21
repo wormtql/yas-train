4bc13ef91bbe3b2a88f53ff68675f65412b827cd
YCR160 <3342711246@qq.com>
2023-08-21 16:51:20 +0800

    Add StarRail data

--- mona/datagen/datagen.py
+++ mona/datagen/datagen.py
@@ -4,13 +4,18 @@ import os
 from PIL import Image, ImageFont, ImageDraw
 
 from mona.text.artifact_name import random_artifact_name
-from mona.text.stat import random_sub_stat, random_main_stat_name, random_main_stat_value
+from mona.text.relic_name import random_relic_name
 from mona.text.characters import random_equip
 from mona.config import config
 from mona.datagen.pre_process import pre_process
 
 # 4k分辨率最大对应84号字，900p分辨率最小对应18号字
-fonts = [ImageFont.truetype("./assets/genshin.ttf", i) for i in range(15, 90)]
+if config["model_type"] == "Genshin":
+    from mona.text.stat_genshin import random_sub_stat, random_main_stat_name, random_main_stat_value
+    fonts = [ImageFont.truetype("./assets/genshin.ttf", i) for i in range(15, 90)]
+elif config["model_type"] == "StarRail":
+    from mona.text.stat_starrail import random_sub_stat, random_main_stat_name, random_main_stat_value
+    fonts = [ImageFont.truetype("./assets/starrail.ttf", i) for i in range(15, 90)]
 
 
 def random_level():
@@ -22,17 +27,24 @@ def random_artifact_count():
     flag_ac = random.randint(0, 1500)
     return f"圣遗物 {flag_ac}/1800"
 
+def random_relic_count():
+    # Random here, for online learning
+    flag_ac = random.randint(0, 1500)
+    return f"遗器数量{flag_ac}/1500"
 
 def random_number():
     n = random.randint(0, 1000000000)
     return f"{n}"
 
-
-random_funcs = [random_artifact_name, random_main_stat_name, random_main_stat_value,
-                random_sub_stat, random_level, random_equip, random_artifact_count, random_number]
-# 加大random_artifact_count的权重，因为连续数字识别是CRNN模型的难点，这对于副词条识别也有帮助。
-random_weights = [0.1, 0.05, 0.15,
-                  0.64, 0.02, 0.02, 0.1, 0.2]
+if config["model_type"] == "Genshin":
+    random_funcs = [random_artifact_name, random_main_stat_name, random_main_stat_value,
+                    random_sub_stat, random_level, random_equip, random_artifact_count, random_number]
+    # 加大random_artifact_count的权重，因为连续数字识别是CRNN模型的难点，这对于副词条识别也有帮助。
+    random_weights = [0.1, 0.05, 0.15, 0.64, 0.02, 0.02, 0.1, 0.2]
+elif config["model_type"] == "StarRail":
+    random_funcs = [random_relic_name, random_main_stat_name, random_main_stat_value,
+                    random_sub_stat, random_level, random_relic_count, random_number]
+    random_weights = [0.1, 0.05, 0.15, 0.64, 0.02, 0.1, 0.2]
 # random_funcs = [
 #     (random_artifact_name, 0.1),
 #     (random_main_stat_name, 0.05),
@@ -123,4 +135,4 @@ def generate_image_sample():
     draw.text((20, 5), "雷素%暴岩1,7.", color2, font=ImageFont.truetype("./assets/genshin.ttf", 80))
 
     img_processed = pre_process(img)
-    return img, img_processed   
+    return img, img_processed
